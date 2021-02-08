29465140811e663167925a014d063ee6fc60e952
Xenon <xzenon@gmail.com>
2021-02-09 00:38:32 +0200

    Added initial console app to test library

---
+++ src/Fingerprint/FingerprintTasks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using SoundFingerprinting;
+using SoundFingerprinting.Audio;
+using SoundFingerprinting.Builder;
+using SoundFingerprinting.Data;
+using SoundFingerprinting.InMemory;
+using SoundFingerprinting.Emy;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fingerprint
+{
+    public class FingerprintTasks
+    {
+        public FingerprintTasks()
+        {
+        }
+
+        private readonly IModelService modelService = new InMemoryModelService(); // store fingerprints in RAM
+
+        //private readonly IAudioService audioService = new SoundFingerprintingAudioService();
+        private readonly IAudioService audioService = new FFmpegAudioService();
+
+        public async Task StoreForLaterRetrieval(string pathToAudioFile)
+        {
+            var track = new TrackInfo("1", "Ta4to", "Boombox");
+
+            // create fingerprints
+            var hashedFingerprints = await FingerprintCommandBuilder.Instance
+                                        .BuildFingerprintCommand()
+                                        .From(pathToAudioFile)
+                                        .UsingServices(audioService)
+                                        .Hash();
+
+            // store hashes in the database for later retrieval
+            modelService.Insert(track, hashedFingerprints);
+
+            Console.WriteLine("StoreForLaterRetrieval:" + modelService.Info.First().ToString());
+        }
+    }
+}
---
+++ src/Fingerprint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Fingerprint
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            FingerprintTasks tasks = new FingerprintTasks();
+            await tasks.StoreForLaterRetrieval(args[0]);
+
+            Console.WriteLine("File: " + args[0]);
+        }
+    }
+}
